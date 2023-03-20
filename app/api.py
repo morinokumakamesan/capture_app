8ccf5b1dd74dff3562995458e25a85e500e3fecb
kumakawa <eiji.kumakawa.ef@nttdocomo.com>
2023-03-20 22:45:02 +0900

    first commit

---
+++ app/api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, make_response, render_template, url_for
+
+from . import service
+
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@api.route('/capture_img', methods=['POST'])
+def capture_img():
+    msg = service.save_img(request.form["img"])
+    return make_response(msg)
+
+@api.route('/hoge')
+def hoge():
+    return render_template('hoge.html')
