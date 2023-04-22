6c07c25f72bbb1ab5f83ff53d952e5b1c0b0cb6f
kumakawa <eiji.kumakawa.ef@nttdocomo.com>
2023-04-22 18:43:33 +0900

    get userId of LINE

--- app/api.py
+++ app/api.py
@@ -51,9 +51,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print('文字を受信')
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.source.userId))
     line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(
