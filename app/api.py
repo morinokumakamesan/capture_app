c94d9c8039d8c2d513d13d39508a7097c3c9753c
kumakawa <eiji.kumakawa.ef@nttdocomo.com>
2023-05-06 20:34:57 +0900

    add multiple access token

--- app/api.py
+++ app/api.py
@@ -24,13 +24,10 @@ def capture_img():
     msg = service.save_img(request.form["img"], request.form["deviceId"])
     return make_response(msg)
 
-line_bot_api = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(config.LINE_CHANNEL_SECRET)
-
-header = {
-    "Content_Type": "application/json",
-    "Authorization": "Bearer " + config.LINE_CHANNEL_ACCESS_TOKEN
-}
+line_bot_api_1 = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
+handler_1 = WebhookHandler(config.LINE_CHANNEL_SECRET)
+line_bot_api_2 = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
+handler_2 = WebhookHandler(config.LINE_CHANNEL_SECRET)
 
 # アプリにPOSTがあったときの処理
 @api.route("/callback", methods=["POST"])
@@ -41,67 +38,24 @@ def callback():
     body = request.get_data(as_text=True)
     api.logger.info("Request body: " + body)
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    for handler in [handler_1, handler_2]:
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+        else:
+            pass
     return "OK"
 
-
 # botにメッセージを送ったときの処理
-@handler.add(MessageEvent, message=TextMessage)
+@handler_1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print('文字を受信')
     image_message = ImageSendMessage(
-        original_content_url = 'https://capture-app.onrender.com/static/images/capture.jpg',
-        preview_image_url = 'https://capture-app.onrender.com/static/images/capture.jpg'
+        original_content_url = 'https://capture-app.onrender.com/static/images/r39rquo416g.jpg',
+        preview_image_url = 'https://capture-app.onrender.com/static/images/r39rquo416g.jpg'
     )
-    pprint(event)
-    pprint(event.source.user_id)
     text_message = TextSendMessage(text=str(event.source.user_id))
-    line_bot_api.reply_message(
+    line_bot_api_1.reply_message(
         event.reply_token, [image_message, text_message]
     )
-    print("返信完了!!\ntext:", event.message.text)
-
-# botに画像を送ったときの処理
-@handler.add(MessageEvent, message=ImageMessage)
-def handle_image(event):
-    print("画像を受信")
-    message_id = event.message.id
-    image_path = getImageLine(message_id)
-    line_bot_api.reply_message(
-        event.reply_token,
-        ImageSendMessage(
-            original_content_url = 'https://capture-app.onrender.com/static/images/capture.jpg',
-            preview_image_url = 'https://capture-app.onrender.com/static/images/capture.jpg'
-        )
-    )
-    print("画像の送信完了!!")
-
-
-# 受信メッセージに添付された画像ファイルを取得
-def getImageLine(id):
-    line_url = f"https://api-data.line.me/v2/bot/message/{id}/content"
-    result = requests.get(line_url, headers=header)
-    print(result)
-
-    img = Image.open(BytesIO(result.content))
-    w, h = img.size
-    if w >= h:
-        ratio_main, ratio_preview = w / 1024, w / 240
-    else:
-        ratio_main, ratio_preview = h / 1024, h / 240
-
-    width_main, width_preview = int(w // ratio_main), int(w // ratio_preview)
-    height_main, height_preview = int(h // ratio_main), int(h // ratio_preview)
-
-    img_main = img.resize((width_main, height_main))
-    img_preview = img.resize((width_preview, height_preview))
-    image_path = {
-        "main": f"./app/static/images/image_{id}_main.jpg",
-        "preview": f"./app/static/images/image_{id}_preview.jpg"
-    }
-    img_main.save(image_path["main"])
-    img_preview.save(image_path["preview"])
-    return image_path
